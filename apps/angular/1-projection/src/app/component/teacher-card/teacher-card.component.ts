c03f2971ca0089932b56f12474b4dcc22646ccf6
PierreChatard <pchatard@monkeyfactory.fr>
2024-12-23 16:20:56 +0100

    refactor: use ng-content and ngTemplateOutlet in card.component.ts

--- apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
+++ apps/angular/1-projection/src/app/component/teacher-card/teacher-card.component.ts
@@ -1,30 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { FakeHttpService } from '../../data-access/fake-http.service';
+import {
+  FakeHttpService,
+  randTeacher,
+} from '../../data-access/fake-http.service';
 import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
 import { Teacher } from '../../model/teacher.model';
 import { CardComponent } from '../../ui/card/card.component';
+import { ListItemComponent } from '../../ui/list-item/list-item.component';
 
 @Component({
   selector: 'app-teacher-card',
   template: `
+    <ng-template #teacherListItemTemplate let-teacher>
+      <app-list-item
+        [id]="teacher.id"
+        [name]="teacher.firstName"
+        (deleteClicked)="delete($event)"></app-list-item>
+    </ng-template>
     <app-card
       [list]="teachers"
-      [type]="cardType"
-      customClass="bg-light-red"></app-card>
+      [listItemTemplate]="teacherListItemTemplate"
+      (addClicked)="add()">
+      <img
+        alt="Teacher image"
+        ngProjectAs="card-image"
+        src="assets/img/teacher.png"
+        width="200px" />
+    </app-card>
   `,
   styles: [
     `
-      ::ng-deep .bg-light-red {
-        background-color: rgba(250, 0, 0, 0.1);
+      :host {
+        --card-background-color: rgba(250, 0, 0, 0.1);
       }
     `,
   ],
-  imports: [CardComponent],
+  imports: [CardComponent, ListItemComponent],
 })
 export class TeacherCardComponent implements OnInit {
   teachers: Teacher[] = [];
-  cardType = CardType.TEACHER;
 
   constructor(
     private http: FakeHttpService,
@@ -35,5 +49,13 @@ export class TeacherCardComponent implements OnInit {
     this.http.fetchTeachers$.subscribe((t) => this.store.addAll(t));
 
     this.store.teachers$.subscribe((t) => (this.teachers = t));
+  }
+
+  delete(id: number) {
+    this.store.deleteOne(id);
+  }
+
+  add() {
+    this.store.addOne(randTeacher());
   }
 }
--- apps/angular/1-projection/src/app/ui/card/card.component.ts
+++ apps/angular/1-projection/src/app/ui/card/card.component.ts
@@ -1,9 +1,11 @@
-import { NgFor, NgIf } from '@angular/common';
-import { Component, Input } from '@angular/core';
-import { randStudent, randTeacher } from '../../data-access/fake-http.service';
-import { StudentStore } from '../../data-access/student.store';
-import { TeacherStore } from '../../data-access/teacher.store';
-import { CardType } from '../../model/card.model';
+import { NgFor, NgTemplateOutlet } from '@angular/common';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  Output,
+  TemplateRef,
+} from '@angular/core';
 import { ListItemComponent } from '../list-item/list-item.component';
 
 @Component({
@@ -12,49 +14,40 @@ import { ListItemComponent } from '../list-item/list-item.component';
     <div
       class="flex w-fit flex-col gap-3 rounded-md border-2 border-black p-4"
       [class]="customClass">
-      <img
-        *ngIf="type === CardType.TEACHER"
-        src="assets/img/teacher.png"
-        width="200px" />
-      <img
-        *ngIf="type === CardType.STUDENT"
-        src="assets/img/student.webp"
-        width="200px" />
+      <ng-content select="card-image"></ng-content>
 
       <section>
-        <app-list-item
+        <ng-container
           *ngFor="let item of list"
-          [name]="item.firstName"
-          [id]="item.id"
-          [type]="type"></app-list-item>
+          [ngTemplateOutlet]="listItemTemplate || defaultTemplate"
+          [ngTemplateOutletContext]="{
+            $implicit: item
+          }"></ng-container>
       </section>
 
       <button
         class="rounded-sm border border-blue-500 bg-blue-300 p-2"
-        (click)="addNewItem()">
+        (click)="addClicked.emit()">
         Add
       </button>
     </div>
+
+    <ng-template #defaultTemplate let-item>
+      <app-list-item [id]="item.id" [name]="item.id"></app-list-item>
+    </ng-template>
   `,
-  imports: [NgIf, NgFor, ListItemComponent],
+  styles: [
+    `
+      :host > div {
+        background-color: var(--card-background-color, white);
+      }
+    `,
+  ],
+  imports: [NgFor, ListItemComponent, NgTemplateOutlet],
 })
-export class CardComponent {
-  @Input() list: any[] | null = null;
-  @Input() type!: CardType;
+export class CardComponent<T extends { id: number }> {
+  @Input() list: T[] | null = null;
+  @Input() listItemTemplate?: TemplateRef<T>;
   @Input() customClass = '';
-
-  CardType = CardType;
-
-  constructor(
-    private teacherStore: TeacherStore,
-    private studentStore: StudentStore,
-  ) {}
-
-  addNewItem() {
-    if (this.type === CardType.TEACHER) {
-      this.teacherStore.addOne(randTeacher());
-    } else if (this.type === CardType.STUDENT) {
-      this.studentStore.addOne(randStudent());
-    }
-  }
+  @Output() addClicked = new EventEmitter();
 }
